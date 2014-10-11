1c01963dc8e14998e446eefbbc207f032ed73ebd
whyisandroid <583417398@qq.com>
2014-10-11 14:20:53 +0800

    提交color

--- gen/com/timetalent/client/R.java
+++ gen/com/timetalent/client/R.java
@@ -17,36 +17,55 @@ public final class R {
     public static final class attr {
     }
     public static final class color {
-        public static final int bggray=0x7f050011;
+        public static final int bantouming=0x7f050004;
+        /**  浅灰 
+         */
+        public static final int bggray=0x7f050012;
         /**  纯白 
          */
-        public static final int black=0x7f050005;
-        public static final int lightgray=0x7f050010;
+        public static final int black=0x7f050006;
+        /**  浅红 
+         */
+        public static final int lightgray=0x7f050011;
         /**  字体颜色标准 
          */
-        public static final int new_color_1=0x7f050009;
-        public static final int new_color_2=0x7f05000a;
-        public static final int new_color_3=0x7f05000b;
-        public static final int new_color_4=0x7f05000c;
-        public static final int new_color_5=0x7f05000d;
-        public static final int new_color_6=0x7f05000e;
+        public static final int new_color_1=0x7f05000a;
+        /**  浅黑 
+         */
+        public static final int new_color_2=0x7f05000b;
+        /**  深灰 
+         */
+        public static final int new_color_3=0x7f05000c;
+        /**  蓝灰 
+         */
+        public static final int new_color_4=0x7f05000d;
+        /**  浅灰 
+         */
+        public static final int new_color_5=0x7f05000e;
+        /**  浅红 
+         */
+        public static final int new_color_6=0x7f05000f;
         public static final int qianhui=0x7f050002;
-        public static final int red=0x7f05000f;
-        public static final int spilt_line_color=0x7f050008;
+        /**  纯白 
+         */
+        public static final int red=0x7f050010;
+        public static final int spilt_line_color=0x7f050009;
         /**  透明色 
          */
-        public static final int tabs_color=0x7f050007;
-        public static final int tabs_text_color=0x7f050013;
+        public static final int tabs_color=0x7f050008;
+        public static final int tabs_text_color=0x7f050014;
         public static final int texthui=0x7f050001;
         public static final int titlebg=0x7f050000;
         public static final int touming=0x7f050003;
         /**  纯黑 
          */
-        public static final int transparent_color=0x7f050006;
+        public static final int transparent_color=0x7f050007;
         /**  定义标准颜色 
          */
-        public static final int white=0x7f050004;
-        public static final int yello_bg_top=0x7f050012;
+        public static final int white=0x7f050005;
+        /**  雾白 
+         */
+        public static final int yello_bg_top=0x7f050013;
     }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 

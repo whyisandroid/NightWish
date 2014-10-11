82478d4a2f7567a0d29fae3a2137e69a04bc888b
fenglianjia <270622805@qq.com>
2014-10-11 15:36:11 +0800

    搜索UI

--- src/com/timetalent/client/ui/fragment/SearchFragment.java
+++ src/com/timetalent/client/ui/fragment/SearchFragment.java
@@ -6,14 +6,12 @@ import java.util.HashMap;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.view.ViewGroup.LayoutParams;
 import android.widget.Button;
-import android.widget.ImageButton;
 import android.widget.ListView;
 import android.widget.PopupWindow;
 import android.widget.TextView;
@@ -32,18 +30,17 @@ import com.timetalent.client.ui.fragment.util.NearBaseAdapter;
  * @time: 2014-10-10 下午6:30:07
  ***************************************** 
  */
-public class NearFragment extends Fragment implements OnClickListener {
+public class SearchFragment extends Fragment implements OnClickListener {
 	private View view;
 	private Context mContext;
 	private AppController controller;
 	private ListView list;
-	private TextView tvshaixuan;
-	private ImageButton imgbtsearch;
+	private TextView tvok;
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 		controller = AppController.getController(getActivity());
-		view = inflater.inflate(R.layout.near_fragment, container, false);
+		view = inflater.inflate(R.layout.near_sousuo, container, false);
 		mContext = getActivity();
 		findView();
 		initView();
@@ -57,9 +54,9 @@ public class NearFragment extends Fragment implements OnClickListener {
 	 * @time: 2014-10-10 下午6:36:02
 	 */
 	private void initView() {
-		list.setAdapter(new NearBaseAdapter(getActivity()));
-		tvshaixuan.setOnClickListener(this);
-		imgbtsearch.setOnClickListener(this);
+		list.setAdapter(new NearBaseAdapter(mContext));
+		list.setVisibility(list.GONE);
+		tvok.setOnClickListener(this);
 	}
 
 	/**
@@ -70,34 +67,14 @@ public class NearFragment extends Fragment implements OnClickListener {
 	 */
 	private void findView() {
 		list = (ListView) view.findViewById(R.id.listView1);
-		tvshaixuan = (TextView) view.findViewById(R.id.tvshaixuan);
-		imgbtsearch = (ImageButton) view.findViewById(R.id.imgbtsearch);
+		tvok = (TextView) view.findViewById(R.id.tvok);
 	}
 
 	@Override
 	public void onClick(View v) {
 		switch (v.getId()) {
-		case R.id.tvshaixuan:
-			LayoutInflater inflater = LayoutInflater.from(view.getContext());
-			View popview = inflater.inflate(R.layout.near_shaixuan, null);
-			final PopupWindow pop = new PopupWindow(popview, LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT, false);
-			pop.setBackgroundDrawable(new Background2());
-			pop.setOutsideTouchable(true);
-			pop.setFocusable(true);
-			Button btok = (Button) popview.findViewById(R.id.btok);
-			btok.setOnClickListener(new OnClickListener() {
-				
-				@Override
-				public void onClick(View v) {
-					pop.dismiss();
-					
-				}
-			});
-			pop.showAsDropDown(v);
-			break;
-		case R.id.imgbtsearch:
-			//NearFragment 跳转dao SearchFragment
-			 
+		case R.id.tvok:
+			list.setVisibility(list.VISIBLE);
 			break;
 		default:
 			break;

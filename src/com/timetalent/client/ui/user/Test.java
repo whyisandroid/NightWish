ddec4b4a81ff7aff239d1f3ffe24b1b0eb45e01a
whyisandroid <583417398@qq.com>
2014-10-11 10:35:03 +0800

    添加包结构

---
+++ src/com/timetalent/client/ui/user/Test.java
@@ -0,0 +1,13 @@
+package com.timetalent.client.ui.user;
+
+
+/******************************************
+ * 类描述： TODO
+ * 类名称：Test  
+ * @version: 1.0
+ * @author: why
+ * @time: 2014-10-11 上午10:29:27 
+ ******************************************/
+public class Test {
+
+}
